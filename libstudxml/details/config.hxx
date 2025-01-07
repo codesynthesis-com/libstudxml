6b83f2f2061310729a22637c7bffec7d1c248304
Boris Kolpackov <boris@codesynthesis.com>
2025-01-07 10:12:32 +0200

    Fix MSVC compatibility issue in config.hxx

--- libstudxml/details/config.hxx
+++ libstudxml/details/config.hxx
@@ -4,8 +4,13 @@
 #ifndef LIBSTUDXML_DETAILS_CONFIG_HXX
 #define LIBSTUDXML_DETAILS_CONFIG_HXX
 
+// Note that MSVC 14.3 (1930) does not define suitable __cplusplus but
+// supports C++11.
+//
 #if !defined(__cplusplus) || __cplusplus < 201103L
-#  error C++11 is required
+#  if !defined(_MSC_VER) || _MSC_VER < 1930
+#    error C++11 is required
+#  endif
 #endif
 
 #ifdef _MSC_VER
